4f9d571764a390df3a69b729848b9afd0bf823e0
SergeLavrentev <bert679@yandex.ru>
2021-03-02 11:12:15 +0400

    Basic file structure

---
+++ src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="wrapper">
+    <h1>I'm Home</h1>
+    <phrase></phrase>
+  </div>
+</template>
+
+<script>
+import Phrase from '@/components/Phrase'
+export default {
+  components: {
+    Phrase
+  }
+}
+</script>
